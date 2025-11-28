ed48737cb44a493e880ae64aaeebd36941882f77
CCastillo26 <charly.castillo2627@gmail.com>
2025-11-28 00:06:51 -0500

    Create simulation.R

---
+++ simulation.R
@@ -0,0 +1,18 @@
+# simulate_arrival_times()
+# Input: lambda(t), lambda_max
+# Output: proposed arrival times t_i
+
+source("estimation.R")
+
+simulate_arrival_times() <- function() {
+  ti <- bike_df %>%
+    
+}
+
+# simulate_days()
+# Input: thinned arrival times
+# Output: inventory by station
+
+simulate_days() <- function() {
+  
+}
