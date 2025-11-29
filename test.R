7f2c45cc3b92b0828f603cc362c0d177ec79009c
CCastillo26 <charly.castillo2627@gmail.com>
2025-11-28 22:26:32 -0500

    Update test.R

---
+++ test.R
@@ -0,0 +1,12 @@
+library(testthat)
+source("estimation.R")
+source("simulation.R")
+source("placement.R")
+
+# Test for simulate_arrival_times()
+
+# Test for simulate_days()
+
+# Test for calculate_metrics()
+
+# Test for optimize_placement()
