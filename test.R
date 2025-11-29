980497d3793a44a20f6f9d61f3ce8f4bf6680632
CCastillo26 <charly.castillo2627@gmail.com>
2025-11-28 23:00:24 -0500

    Update test.R

--- test.R
+++ test.R
@@ -3,10 +3,37 @@ source("estimation.R")
 source("simulation.R")
 source("placement.R")
 
-# Test for simulate_arrival_times()
+# TEST 1: simulate_arrival_times()
+toy_arrival_rates <- data.frame(start_station = "5", 
+                                end_station = "10", hour = 0, avg_trips = 1,
+                                avg_avail = 1, mu_hat = 1)
 
-# Test for simulate_days()
+toy_arrivals <- simulate_arrival_times(toy_arrival_rates, day = 24)
 
-# Test for calculate_metrics()
+# Runs successfully without error
+expect_true(all(toy_arrivals$time >= 0 & toy_arrivals$time <= 24))
+expect_true(all(toy_arrivals$start_station == "10"))
 
-# Test for optimize_placement()
+
+
+# TEST 2: simulate_days()
+arrivals <- data.frame(start_station = c("5", "5"), time = c(1, 2))
+initial_bikes <- c("5" = 1)
+
+result <- simulate_days(arrivals, initial_bikes)
+
+trip_log <- result$trip_log
+final_inventory <- result$final_inventory
+
+# Runs successfully without error
+expect_true(sum(final_inventory) <= sum(initial_bikes))
+expect_equal(length(trip_log$success), nrow(arrivals))
+
+
+
+# TEST 3: calculate_metrics()
+
+
+
+
+# TEST 4: optimize_placement()
